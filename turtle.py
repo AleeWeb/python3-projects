3813a51d61f7482b9537bdd15a4fdacebcbf8cec
Alicia Alexander <aliciaalexander@Alicias-MacBook-Pro.local>
2017-10-25 06:46:48 -0400

    Updated turtle and variables files.

--- turtle.py
+++ turtle.py
@@ -1,4 +1,8 @@
 import turtle
 
-my_turtle = turtle.Turtle()
-my_turtle.forward(100)
+wn = turtle.Screen()
+wn.bgcolor('green')
+
+Franklin = turtle.Turtle()
+Franklin.speed(0)
+Franklin.color('white')
---
+++ variables.py
@@ -0,0 +1,12 @@
+
+
+# Variables and Strings in PY 3
+
+michael = "61 years old, 5'11 and is retired"
+print (michael)
+
+age = 31
+print (age)
+
+p3 = "This is Python 3"
+print(p3)
